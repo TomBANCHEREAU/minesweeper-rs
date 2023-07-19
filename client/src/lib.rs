ec2893eb0f1c26c0580be19bb88ef4bbb23c3bf9
TomBANCHEREAU <banchereau.tom@gmail.com>
2023-07-19 12:32:14 +0200

    security: remove wee_alloc

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -11,17 +11,6 @@ use viewport::ViewportOptions;
 
 use wasm_bindgen::prelude::*;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-// #[wasm_bindgen]
-// extern "C" {
-//     fn alert(s: &str);
-// }
-
 #[wasm_bindgen]
 pub fn start(canvas_id: String, lobby_id: String) {
     set_panic_hook();
